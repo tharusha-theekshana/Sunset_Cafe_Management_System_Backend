9e53f621f2b676edf9d1492e3745caeea78b6718
Tharusha Theekshana <tharusha@fh.technology>
2023-08-10 09:54:47 +0530

    Generated Project

---
+++ src/main/java/com/sunset_cafe/sunset_cafe_backend/SunsetCafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunset_cafe.sunset_cafe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsetCafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunsetCafeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunset_cafe/sunset_cafe_backend/SunsetCafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunset_cafe.sunset_cafe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsetCafeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
